59b1f8f912a1cf27f12da95d9beafbc7bf2c4d4d
Alex Ruaux <alex@molecularcodewebdesign.com>
2016-04-13 18:31:21 +0000

    function6 - do math based on the opperator inputted

--- answers.js
+++ answers.js
@@ -63,4 +63,32 @@ function multiply(num1, num2) {
         return("enter two numbers");
     }
 };
-console.log(multiply(5, 8));
+//console.log(multiply(5, 8));
+
+
+// 6. Write a function that takes two numbers and a string. If the string is ‘add’, then return the sum of the numbers. If the string is ‘subtract’, return the difference. If the string is ‘mult’, return the product. If the string is ‘div’, return the ratio. Otherwise return 0.
+
+function math(num1, num2, opperator) {
+    /* when passed something other than 2 variables of type number followed by a variable of type string, the user is prompted to enter 2 numbers followed by the opperator */
+    if(typeof num1 === "number" && typeof num2 === "number" && typeof opperator === "string") {
+        if(opperator === "add") {
+            return(num1 + num2);   
+        }
+        else if(opperator === "subtract") {
+            return(num1 - num2);   
+        }
+        else if(opperator === "mult") {
+            return(num1 * num2);   
+        }
+        else if(opperator === "div") {
+            return(num1 / num2);   
+        }
+        else {
+            return("0 - enter the opperator as one of: add, subtract, mult, div");
+        }
+    }
+    else {
+        return("enter two numbers followed by the opperator (add, subtract, mult, div) as csv");
+    }
+};
+console.log(math(12, 4, "mult"));
