20c82d1fe9ca42b95d40bfe94f676fab1808613b
Alex Ruaux <alex@molecularcodewebdesign.com>
2016-04-13 17:36:23 +0000

    function1 - first char of string

--- answers.js
+++ answers.js
@@ -1,1 +1,12 @@
-// Answers
+// cd-day3-javscript-intro: Alex's Javascript Functions Answers
+
+// 1. Write a function that takes a string and returns the first character of the string. Test your function on a few inputs, including the empty string.
+function firstChar(string) {
+    if(string) {
+        console.log(string.charAt(0));
+    }
+    else {
+        console.log("enter a string");
+    }
+};
+firstChar("fred");
