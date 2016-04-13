50655fc6f5224726599f42e5e38389dc06f8dce5
Alex Ruaux <alex@molecularcodewebdesign.com>
2016-04-13 17:43:34 +0000

    function2 - last char of string

--- answers.js
+++ answers.js
@@ -1,6 +1,8 @@
 // cd-day3-javscript-intro: Alex's Javascript Functions Answers
 
+
 // 1. Write a function that takes a string and returns the first character of the string. Test your function on a few inputs, including the empty string.
+
 function firstChar(string) {
     if(string) {
         console.log(string.charAt(0));
@@ -9,4 +11,18 @@ function firstChar(string) {
         console.log("enter a string");
     }
 };
-firstChar("fred");
+firstChar("Fred");
+
+
+// Write a function that takes a string and returns the last character of a string. Test your function on a few inputs, including the empty string.
+
+function lastChar(string) {
+    if(string) {
+        console.log(string.slice(-1));
+    }
+    else {
+        console.log("enter a string");
+    }
+};
+lastChar("Albert");
+
